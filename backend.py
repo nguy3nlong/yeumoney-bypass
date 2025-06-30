e146431ad6c54cac9f44d9bc1ce6ab8e2b343131
Reno <longnguyenstocks@gmail.com>
2025-06-30 17:53:11 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -12,11 +12,9 @@ def k():
     type = json['type']
     if not type:
         return jsonify({'error': 'get the fuck out bitch'}), 400
-    clk = json['clk']
-    if not clk:
-        return jsonify({'error': 'get the fuck out bitch'}), 400
+    
     if type == 'm88':
-        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrai&url=https://bet88ec.com/cach-danh-bai-sam-loc&loai_traffic=https://bet88ec.com/&clk={clk}')
+        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrai&url=https://bet88ec.com/cach-danh-bai-sam-loc&loai_traffic=https://bet88ec.com/&clk=1000')
         html = response.text
         match = re.search(r'<span id="layma_me_vuatraffic"[^>]*>\s*(\d+)\s*</span>', html)
         if match:
@@ -26,7 +24,7 @@ def k():
             return jsonify({'error': 'cannot get code'}), 400
 
     if type == 'fb88':
-        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrai&url=https://fb88mg.com/ty-le-cuoc-hong-kong-la-gi&loai_traffic=https://fb88mg.com/&clk={clk}')
+        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrai&url=https://fb88mg.com/ty-le-cuoc-hong-kong-la-gi&loai_traffic=https://fb88mg.com/&clk=1000')
         html = response.text
         match = re.search(r'<span id="layma_me_vuatraffic"[^>]*>\s*(\d+)\s*</span>', html)
         if match:
@@ -36,7 +34,7 @@ def k():
             return jsonify({'error': 'cannot get code'}), 400
 
     if type == '188bet':
-        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrailamnhe&url=https://88betag.com/cach-choi-game-bai-pok-deng&loai_traffic=https://88betag.com/&clk={clk}')
+        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrailamnhe&url=https://88betag.com/cach-choi-game-bai-pok-deng&loai_traffic=https://88betag.com/&clk=1000')
         html = response.text
         match = re.search(r'<span id="layma_me_vuatraffic"[^>]*>\s*(\d+)\s*</span>', html)
         if match:
@@ -46,7 +44,7 @@ def k():
             return jsonify({'error': 'cannot get code'}), 400
 
     if type == 'w88':
-        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrai&url=https://188.166.185.213/tim-hieu-khai-niem-3-bet-trong-poker-la-gi&loai_traffic=https://188.166.185.213/&clk={clk}')
+        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=taodeptrai&url=https://188.166.185.213/tim-hieu-khai-niem-3-bet-trong-poker-la-gi&loai_traffic=https://188.166.185.213/&clk=1000')
         html = response.text
         match = re.search(r'<span id="layma_me_vuatraffic"[^>]*>\s*(\d+)\s*</span>', html)
         if match:
