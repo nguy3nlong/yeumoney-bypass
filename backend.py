5649daaae4fca470297365cf57a502d6d53f0178
Reno <longnguyenstocks@gmail.com>
2025-07-12 16:24:07 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -74,7 +74,7 @@ def k():
             return jsonify({'error': 'cannot get code'}), 400
 
     if type == 'vn88':
-        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=bodeptrai&url=https://vn88no.com/keo-chap-1-trai-la-gi&loai_traffic=https://vn88no.com/&clk=1000')
+        response = requests.post(f'https://traffic-user.net/GET_MA.php?codexn=deobiet&url=https://vn88no.com/game-quan-ly-bong-da-chat-luong&loai_traffic=https://vn88no.com/&clk=1000')
         html = response.text
         match = re.search(r'<span id="layma_me_vuatraffic"[^>]*>\s*(\d+)\s*</span>', html)
         if match:
